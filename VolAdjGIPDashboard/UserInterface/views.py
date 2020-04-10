cf0ef6d767778ca94e56cab71e23f2c5fe6dce61
Matthew Scheffel <mscheffel@gmail.com>
2020-04-10 16:49:32 -0400

    Tweaks to quad start/end

--- VolAdjGIPDashboard/UserInterface/views.py
+++ VolAdjGIPDashboard/UserInterface/views.py
@@ -90,7 +90,8 @@ def index(request, default_net_liquidating_value=10000, lookback=28, default_cur
     prior_quad_return = dict()
     
     data_updated = YahooHistory.objects.latest('updated').updated
-    prior_quad_end_date = QuadForecasts.objects.exclude(quarter_end_date__gte=quarter_end_date).filter(date__lte=F('quarter_end_date')).latest('quarter_end_date', 'date').date
+    #prior_quad_end_date = QuadForecasts.objects.exclude(quarter_end_date__gte=quarter_end_date).filter(date__lte=F('quarter_end_date')).latest('quarter_end_date', 'date').date
+    prior_quad_end_date = (quarter_end_date - pd.tseries.offsets.QuarterEnd()).date()
 
     for quad in quad_allocation:
         try_date = datetime.date.today()
@@ -127,15 +128,16 @@ def index(request, default_net_liquidating_value=10000, lookback=28, default_cur
         quad_allocations[quad] = YahooHistory.equal_volatility_position(quad_allocation[quad], target_value=net_liquidating_value)
 
     # FIXME should be an unnecessary try/catch
-    try:
-        current_quad_start = QuadReturn.objects.latest('quarter_end_date', 'data_end_date').data_start_date
-    except QuadReturn.DoesNotExist:
-        current_quad_start = '?'
+    # try:
+    #     current_quad_start = QuadReturn.objects.latest('quarter_end_date', 'data_end_date').data_start_date
+    # except QuadReturn.DoesNotExist:
+    #     current_quad_start = '?'
+    current_quad_start = (quarter_end_date - pd.tseries.offsets.QuarterBegin(2)).date()
     
     try:
-        prior_quad = QuadReturn.objects.filter(data_end_date__lt=current_quad_start).latest('quarter_end_date', 'data_end_date')
+        prior_quad = QuadReturn.objects.filter(quarter_end_date=prior_quad_end_date).latest('quarter_end_date', 'data_end_date')
         prior_quad_start = prior_quad.data_start_date
-        prior_quad_end = prior_quad.data_end_date
+        prior_quad_end = current_quad_start - datetime.timedelta(days=1)
     except QuadReturn.DoesNotExist:
         prior_quad_start, prior_quad_end = '?', '?'
 
