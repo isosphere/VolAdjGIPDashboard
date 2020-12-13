30227fa01a80aff1f65db8e0427110a0d8782075
Matthew Scheffel <mscheffel@gmail.com>
2020-12-13 18:42:09 -0500

    This migration never worked due to database locking issues in an atomic migration transaction, wipe it

--- VolAdjGIPDashboard/DataAcquisition/migrations/0019_auto_20201204_1928.py
+++ VolAdjGIPDashboard/DataAcquisition/migrations/0019_auto_20201204_1928.py
@@ -2,21 +2,6 @@
 
 from django.db import migrations
 
-def forwards_func(apps, schema_edtior):
-    #YahooHistory = apps.get_model("DataAcquisition", "YahooHistory")
-    #QuadReturn = apps.get_model("DataAcquisition", "QuadReturn")
-    from DataAcquisition.models import YahooHistory, QuadReturn # bad practice, but only way to access class methods
-
-    print("Force updating YahooHistory symbols ...")
-    YahooHistory.update(tickers=['QQQ', 'XLI', 'XLF', 'GLD', 'XLU', 'UUP', 'TLT'], clobber=True)
-
-    print("Wiping out QuadReturn data ...")
-    QuadReturn.objects.all().delete()
-
-    print("Recalculating QuadReturn data ...")
-    QuadReturn.update()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -24,5 +9,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(forwards_func, migrations.RunPython.noop)
+        migrations.RunPython(migrations.RunPython.noop, migrations.RunPython.noop)
     ]
