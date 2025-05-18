3921111be12b01c4eb82a499cce149f35ab9146a
Matthew Scheffel <matt@dataheck.com>
2025-05-18 15:21:32 -0300

    edit: help message

--- VolAdjGIPDashboard/DataAcquisition/management/commands/update_data.py
+++ VolAdjGIPDashboard/DataAcquisition/management/commands/update_data.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from DataAcquisition.models import YahooHistory, QuadForecasts, QuadReturn, CPIForecast, GDPForecast
 
 class Command(BaseCommand):
-    help = 'Update security history and forecasts'
+    help = 'Update security history and forecasts. This command is sufficient for an empty database, or an out-of-date one.'
 
     def handle(self, *args, **options):
         CPIForecast.update()
